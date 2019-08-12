3236fac53bc8a582118bd0fe96d7aaee7391d499
Hamilton <ehamiltonhudson@gmail.com>
2019-08-11 20:15:53 -0400

    problem #03 solved

---
+++ node_school-learnyounode/03-MyFirstIO/MyFirstIO.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+var filePath = process.argv[2]
+
+function myFirstIO() {
+  let fileData = fs.readFileSync(process.argv[2], 'utf8')
+  let newLines = fileData.toString().split('\n')
+  console.log(newLines.length - 1)
+}
+
+myFirstIO()
