50c3369d6212d8a2e72b2849115b664b1c214216
Hamilton <ehamiltonhudson@gmail.com>
2019-08-11 20:36:16 -0400

    problem #02 solved

--- node_school-learnyounode/02-BabySteps/BabySteps.js
+++ node_school-learnyounode/02-BabySteps/BabySteps.js
@@ -2,7 +2,7 @@ function babySteps() {
   let nums = process.argv.slice(2)
   let sum = 0
   nums.forEach(num => sum = sum + Number(num))
-  console.log(sum)
+  return console.log(sum)
 }
 
 babySteps()
